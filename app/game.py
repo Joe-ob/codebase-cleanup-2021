1efa23ae20c84ba51a24ff2f44497b2a4908aef9
JoeObrien <jpo52@georgetown.edu>
2021-04-01 13:11:06 -0500

    simplified game results
    Output simplified, I still need to verify all outcomes work and will uncomment and delete the old version then

--- app/game.py
+++ app/game.py
@@ -22,23 +22,66 @@ print("COMPUTER CHOICE:", c)
 # DETERMINATION OF WINNER
 #
 
-if u == "rock" and c == "rock":
-    print("It's a tie!")
-elif u == "rock" and c == "paper":
-    print("The computer wins")
-elif u == "rock" and c == "scissors":
-    print("The user wins")
+#if u == "rock" and c == "rock":
+#    print("It's a tie!")
+#elif u == "rock" and c == "paper":
+#    print("The computer wins")
+#elif u == "rock" and c == "scissors":
+#    print("The user wins")
+#
+#elif u == "paper" and c == "rock":
+#    print("The computer wins")
+#elif u == "paper" and c == "paper":
+#    print("It's a tie!")
+#elif u == "paper" and c == "scissors":
+#    print("The user wins")
+#
+#elif u == "scissors" and c == "rock":
+#    print("The computer wins")
+#elif u == "scissors" and c == "paper":
+#    print("The user wins")
+#elif u == "scissors" and c == "scissors":
+#    print("It's a tie!")
 
-elif u == "paper" and c == "rock":
-    print("The computer wins")
-elif u == "paper" and c == "paper":
-    print("It's a tie!")
-elif u == "paper" and c == "scissors":
-    print("The user wins")
 
-elif u == "scissors" and c == "rock":
-    print("The computer wins")
-elif u == "scissors" and c == "paper":
-    print("The user wins")
-elif u == "scissors" and c == "scissors":
-    print("It's a tie!")
+if u == c:
+    print("It's a Tie")
+if u == "rock":
+    if c == "paper":
+        print("The Computer Wins")
+    elif c == "scissors":
+        print("The User Wins")
+if u == "paper":
+    if c == "rock":
+        print("The User Wins")
+    elif c == "scissors":
+        print("The Computer Wins")
+if u == "scissors":
+    if c == "rock":
+        print("The Computer Wins")
+    elif c == "paper":
+        print("The User Wins")
+
+
+
+#if u == "rock":
+#    if c == "rock":
+#        print("It's a Tie")
+#    elif c == "paper":
+#        print("The Computer Wins")
+#    elif c == "scissors":
+#        print("The User Wins")
+#if u == "paper":
+#    if c == "rock":
+#        print("The User Wins")
+#    elif c == "paper":
+#        print("It's a Tie")
+#    elif c == "scissors":
+#        print("The Computer Wins")
+#if u == "scissors":
+#    if c == "rock":
+#        print("The Computer Wins")
+#    elif c == "paper":
+#        print("The User Wins")
+#    elif c == "scissors":
+#        print("It's a Tie")
